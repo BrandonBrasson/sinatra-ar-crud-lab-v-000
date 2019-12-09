e71d2a5cbf9c12733359a0aeb24f721c38b9648b
Brandon Brasson <rocksong622@gmail.com>
2019-12-09 21:24:46 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,7 +38,7 @@ class ApplicationController < Sinatra::Base
   patch '/articles/:id' do
   @article = Article.find(params[:id])
   @article.update(params[:article])
-  redirect “/@article.id”>articles/{@book.id}”
+  redirect  '/@article.id”>articles/{@book.id}'
   end
 
 
